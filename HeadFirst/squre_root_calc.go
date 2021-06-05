6b403191d7474e934fdcfb717d64b6f3133ae958
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-06-05 17:43:57 +0600

    Little additions from Head First Book

---
+++ HeadFirst/squre_root_calc.go
@@ -0,0 +1,23 @@
+//This function returns square root of input number (in real, not in complex field)
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func squareRoot(number float64) (float64, error) {
+	if number < 0 {
+		return 0, fmt.Errorf("can't get square root of negative number")
+	}
+	return math.Sqrt(number), nil
+}
+
+func main() {
+	root, err := squareRoot(100)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%0.3f", root)
+	}
+}
