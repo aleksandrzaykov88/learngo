b0f0022bdac5d25e6492d993fcdff305339dbb45
aleksandrzaykov <aleksandrzaykov88@gmail.com>
2021-06-06 17:14:47 +0600

    Website Go learning and start quadratic equation task

--- GoWebsite/main.go
+++ GoWebsite/main.go
@@ -7,6 +7,8 @@ import (
 
 	"database/sql"
 
+	"github.com/gorilla/mux"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -16,6 +18,7 @@ type Article struct {
 }
 
 var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -82,11 +85,46 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func show_post(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	vars := mux.Vars(r)
+	db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("SELECT *  FROM `articles` WHERE `id` = '%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+	showPost = Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Announcement, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		showPost = post
+	}
+
+	t.ExecuteTemplate(w, "show", showPost)
+}
+
 func handleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET", "POST")
+
+	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
---
+++ SimpleJavaTasks/quadratic_equation.go
@@ -0,0 +1,10 @@
+//Program gets on its input three real numbers. It is coefficients of quadratic equation. Program finds those roots and returns their amount
+package main
+
+import (
+	"fmt"
+)
+
+func quadraticEquationRootsCalc(a, b, c float64) {
+	fmt.Println(a, b, c)
+}
