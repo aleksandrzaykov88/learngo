8c086d735de32f24f50ab9a828d3fc9e5ebbd882
Александр Хэльский <aleksandrzaykov88@gmail.com>
2021-06-21 10:23:04 +0600

    I have practiced creating custom types.

--- HeadFirst/main.go
+++ HeadFirst/main.go
@@ -16,5 +16,6 @@ func main() {
 	//readFile()
 	//fmt.Println(maximum(12, 12313, -2, 3, 0.3))
 	//fmt.Println(inRange(0, 22, -1, 11, 2, 34, 13, 43, 12))
-	countStrings()
+	//countStrings()
+	liquidVolumes()
 }
