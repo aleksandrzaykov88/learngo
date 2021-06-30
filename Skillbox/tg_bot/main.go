5b7cdd1f569d40442a005c6a4e275db3d73aeb04
Александр Хэльский <aleksandrzaykov88@gmail.com>
2021-06-30 08:25:03 +0600

    I finished TG bot creation homework

--- Skillbox/tg_bot/main.go
+++ Skillbox/tg_bot/main.go
@@ -12,19 +12,43 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+const (
+	USD string = "USD"
+	RUB string = "RUB"
+)
+
 type bnResp struct {
-	Price float64 `json"price,string"`
-	Code  int64   `json"code"`
+	Price float64 `json:"price,string"`
+	Code  int64   `json:"code"`
 }
 
 type wallet map[string]float64
 
 var db = map[int64]wallet{}
 
+func exchangeRate(curFrom string, curTo string) (amountTo float64, err error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%sT%s", curFrom, curTo))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var jsonResp bnResp
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if jsonResp.Code != 0 {
+		err = errors.New("Неверный символ")
+	}
+	amountTo = jsonResp.Price
+	return
+}
+
 func getPrice(symbol string) (price float64, err error) {
 	resp, err := http.Get(fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%sUSDT", symbol))
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	defer resp.Body.Close()
@@ -33,7 +57,7 @@ func getPrice(symbol string) (price float64, err error) {
 
 	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	if jsonResp.Code != 0 {
@@ -49,7 +73,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -57,6 +81,8 @@ func main() {
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
+
+	rubTUsd, _ := exchangeRate(USD, RUB)
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
@@ -105,9 +131,9 @@ func main() {
 			for key, value := range db[update.Message.Chat.ID] {
 				price, _ := getPrice(key)
 				sum += value * price
-				msg += fmt.Sprintf("%s: %f [%.2f]\n", key, value, value*price)
+				msg += fmt.Sprintf("%s: %f [%.2fUSD/%.2fRUB]\n", key, value, value*price, rubTUsd*value*price)
 			}
-			msg += fmt.Sprintf("Total: %.2f\n", sum)
+			msg += fmt.Sprintf("Total: [%.2fUSD/%.2fRUB]\n", sum, sum*rubTUsd)
 			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 		default:
 			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда не найдена"))
