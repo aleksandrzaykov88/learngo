8271a32d163a874591607f105f6e656df494d2dc
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-07-07 16:09:26 +0600

    Goroutines-chapter from Zashkevish book has been over. Let go doing tasks!

--- Zhashkevych/goroutines_training/main.go
+++ Zhashkevych/goroutines_training/main.go
@@ -6,26 +6,22 @@ import (
 )
 
 func main() {
+	result1 := make(chan int)
+	result2 := make(chan int)
+
 	t := time.Now()
 	fmt.Printf("Start: %s\n", t.Format(time.RFC3339))
 
-	go func() {
-		for {
-			for _, r := range `—\|/` {
-				fmt.Printf("\r%c", r)
-				time.Sleep(time.Millisecond * 100)
-			}
-		}
-	}()
+	go calculateSmthn(1000, result1)
+	go calculateSmthn(2000, result2)
 
-	go calculateSmthn(1000)
-	go calculateSmthn(1000)
+	fmt.Println(<-result1)
+	fmt.Println(<-result2)
 
-	time.Sleep(8 * time.Second)
 	fmt.Printf("Time: %s\n", time.Since(t))
 }
 
-func calculateSmthn(n int) {
+func calculateSmthn(n int, res chan int) {
 	t := time.Now()
 
 	result := 0
@@ -36,4 +32,5 @@ func calculateSmthn(n int) {
 	}
 
 	fmt.Printf("Result: %d; Time passed: %s\n", result, time.Since(t))
+	res <- result
 }
