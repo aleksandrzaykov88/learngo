ec351e63f0d34d8367f74751806e8e9e40dd258c
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-07-14 21:48:15 +0600

    Add new example from Summerfield's book.

--- Summerfield/wordfrequency/main.go
+++ Summerfield/wordfrequency/main.go
@@ -8,9 +8,63 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sort"
 	"strings"
+	"unicode"
 	"unicode/utf8"
 )
+
+//reportByFrequency outputs data in such a way that corresponding words are output for each frequency value.
+func reportByFrequency(wordsForFrequency map[int][]string) {
+	frequencies := make([]int, 0, len(wordsForFrequency))
+	for frequency := range wordsForFrequency {
+		frequencies = append(frequencies, frequency)
+	}
+	sort.Ints(frequencies)
+	width := len(fmt.Sprint(frequencies[len(frequencies)-1]))
+	fmt.Println("Frequency → Words")
+	for _, frequency := range frequencies {
+		words := wordsForFrequency[frequency]
+		sort.Strings(words)
+		fmt.Printf("%*d %s\n", width, frequency, strings.Join(words, ", "))
+	}
+}
+
+//invertStringIntMap inverts keys and values in map.
+func invertStringIntMap(intForString map[string]int) map[int][]string {
+	stringsForInt := make(map[int][]string, len(intForString))
+	for key, value := range intForString {
+		stringsForInt[value] = append(stringsForInt[value], key)
+	}
+	return stringsForInt
+}
+
+//reportByWords outputs data in such a way that for each word its frequency is displayed.
+func reportByWords(frequencyForWord map[string]int) {
+	words := make([]string, 0, len(frequencyForWord))
+	wordWidth, frequencyWidth := 0, 0
+	for word, frequency := range frequencyForWord {
+		words = append(words, word)
+		if width := utf8.RuneCountInString(word); width > wordWidth {
+			wordWidth = width
+		}
+		if width := len(fmt.Sprint(frequency)); width > frequencyWidth {
+			frequencyWidth = width
+		}
+	}
+	sort.Strings(words)
+	gap := wordWidth + frequencyWidth - len("Word") - len("Frequency")
+	fmt.Printf("Word %*s%s\n", gap, " ", "Frequency")
+	for _, word := range words {
+		fmt.Printf("%-*s %*d\n", wordWidth, word, frequencyWidth, frequencyForWord[word])
+	}
+}
+
+//SplitOnNonLetters split string by no a letter chars.
+func SplitOnNonLetters(s string) []string {
+	notALetter := func(char rune) bool { return !unicode.IsLetter(char) }
+	return strings.FieldsFunc(s, notALetter)
+}
 
 //readAndUpdateFrequencies reads file and updates frequencies for words.
 func readAndUpdateFrequencies(reader *bufio.Reader, frequencyForWord map[string]int) {
@@ -60,14 +114,17 @@ func commandLineFiles(files []string) []string {
 }
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "-help" {
-		fmt.Printf("usage: %s <file1> [<file2> [... <fileN>]]\n", filepath.Base(os.Args[0]))
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <file1> [<file2> [... <fileN>]]\n",
+			filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
+
 	frequencyForWord := map[string]int{}
 	for _, filename := range commandLineFiles(os.Args[1:]) {
 		updateFrequencies(filename, frequencyForWord)
 	}
+	reportByWords(frequencyForWord)
 	wordsForFrequency := invertStringIntMap(frequencyForWord)
 	reportByFrequency(wordsForFrequency)
 }
