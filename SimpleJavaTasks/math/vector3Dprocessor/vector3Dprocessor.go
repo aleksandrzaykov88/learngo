ebf8bea20471f85c87a3aa46e93256f05f6d4660
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-07-17 18:37:11 +0600

    ++++

--- SimpleJavaTasks/math/vector3Dprocessor/vector3Dprocessor.go
+++ SimpleJavaTasks/math/vector3Dprocessor/vector3Dprocessor.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/aleksandrzaykov88/learngo/SimpleJavaTasks/math/vector3D"
+	"github.com/aleksandrzaykov88/learngo/SimpleJavaTasks/math/vector3Dn"
 )
 
 type Vector3DProcessor struct {
-	v, w vector3D.Vector3D
+	v, w vector3Dn.Vector3D
 }
 
-func (p *Vector3DProcessor) SumVectors() vector3D.Vector3D {
+func (p *Vector3DProcessor) SumVectors() vector3Dn.Vector3D {
 	return vector3D.NewVector3DCoords(p.v.X()+p.w.X(), p.v.Y()+p.w.Y(), p.v.Z()+p.w.Z())
 }
 
 func main() {
-	v := vector3D.NewVector3DCoords(1, 3, 7)
-	w := vector3D.NewVector3DCoords(33, 11, 27)
+	v := vector3Dn.NewVector3DCoords(1, 3, 7)
+	w := vector3Dn.NewVector3DCoords(33, 11, 27)
 	vp := Vector3DProcessor{v, w}
 	fmt.Println(vp.SumVectors())
 }
