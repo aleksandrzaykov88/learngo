6834e407f8bc77fc86445224f9007435854cbe84
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-07-17 22:27:59 +0600

    Vector3DProcessor class added

---
+++ SimpleJavaTasks/math_func/vector3Dprocessor/go.mod
@@ -0,0 +1,5 @@
+module vector_proc
+
+go 1.16
+
+require github.com/aleksandrzaykov88/learngo v0.0.0-20210717124124-517b7c840769
--- SimpleJavaTasks/math_func/vector3Dprocessor/vector3Dprocessor.go
+++ SimpleJavaTasks/math_func/vector3Dprocessor/vector3Dprocessor.go
@@ -11,12 +11,12 @@ type Vector3DProcessor struct {
 }
 
 func (p *Vector3DProcessor) SumVectors() vector3D.Vector3D {
-	return vector3D.NewVector3DCoords(p.v.X()+p.w.X(), p.v.Y()+p.w.Y(), p.v.Z()+p.w.Z())
+	return *vector3D.NewVector3DCoords(p.v.X()+p.w.X(), p.v.Y()+p.w.Y(), p.v.Z()+p.w.Z())
 }
 
 func main() {
 	v := vector3D.NewVector3DCoords(1, 3, 7)
 	w := vector3D.NewVector3DCoords(33, 11, 27)
-	vp := Vector3DProcessor{v, w}
+	vp := Vector3DProcessor{*v, *w}
 	fmt.Println(vp.SumVectors())
 }
