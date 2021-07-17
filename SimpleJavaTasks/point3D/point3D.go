5a102f3380322d56a1faadd695d0596a5807137d
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-07-17 12:09:10 +0600

    ++

---
+++ SimpleJavaTasks/point3D/point3D.go
@@ -0,0 +1,48 @@
+package point3D
+
+import "fmt"
+
+type Point3D struct {
+	x, y, z float64
+}
+
+func NewPoint3D() *Point3D {
+	return &Point3D{}
+}
+
+func (p *Point3D) SetX(x float64) {
+	p.x = x
+}
+
+func (p *Point3D) SetY(y float64) {
+	p.y = y
+}
+
+func (p *Point3D) SetZ(z float64) {
+	p.z = z
+}
+
+func (p *Point3D) X() float64 {
+	return p.x
+}
+
+func (p *Point3D) Y() float64 {
+	return p.y
+}
+
+func (p *Point3D) Z() float64 {
+	return p.z
+}
+
+//PrintPoint outputs point coordinates.
+func (p *Point3D) PrintPoint() {
+	fmt.Printf("Point coordinates: X=%.02f Y=%.02f Z=%.02f", p.x, p.y, p.z)
+}
+
+//pointsEquals returns true if coordinates of two input points are equal.
+func pointsEquals(A, B Point3D) bool {
+	if A.x == B.x && A.y == B.y && A.z == B.z {
+		return true
+	}
+	return false
+}
--- SimpleJavaTasks/vector3D/go.mod
+++ SimpleJavaTasks/vector3D/go.mod
@@ -2,4 +2,4 @@ module v
 
 go 1.16
 
-require github.com/aleksandrzaykov88/learngo v0.0.0-20210716154029-bc40a2afaf18
+require github.com/aleksandrzaykov88/learngo latest
