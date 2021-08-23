0c50ba776f0f7b319501fd6bd7712e94a5a348e5
aleksandrzaykov88 <aleksandrzaykov88@gmail.com>
2021-08-23 10:36:47 +0600

    Start doing second homework from Coursera.

---
+++ Coursera/signer/signer.go
@@ -0,0 +1,35 @@
+package main
+
+//ExecutePipeline provides pipelining of worker functions.
+func ExecutePipeline() {
+
+}
+
+//SingleHash calculates value crc32(data)+"~"+crc32(md5(data)).
+func SingleHash() {
+
+}
+
+//MultiHash calculates value crc32(th+data)).
+func MultiHash() {
+
+}
+
+//CombineResults gets all results, sorts, combines the sorted result with _ into one line.
+func CombineResults() {
+
+}
+
+//DataSignerCrc32 returns crc32-value.
+func DataSignerCrc32() {
+
+}
+
+//DataSignerMd5 returns md5-value.
+func DataSignerMd5() {
+
+}
+
+func main() {
+
+}
