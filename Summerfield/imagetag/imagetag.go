96e4ea3165c3dd633633a7d98f8db2e64293064d
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2021-11-08 21:50:18 +0300

    Sizeimages: added regexp on <img>

--- Summerfield/imagetag/imagetag.go
+++ Summerfield/imagetag/imagetag.go
@@ -12,6 +12,8 @@ import (
 	_ "image/png"
 )
 
+var workers = runtime.NumCPU()
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
@@ -21,28 +23,59 @@ func main() {
 	}
 
 	files := commandLineFiles(os.Args[1:])
+	jobs := make(chan string, workers*16)
+	results := make(chan string)
+	done := make(chan struct{}, workers)
+
+	go addJobs(files, jobs)
+	for i := 0; i < workers; i++ {
+		go doJobs(done, results, jobs)
+	}
+	waitAndProcessResults(done, results)
+}
+
+func addJobs(files []string, jobs chan string) {
 	for _, filename := range files {
-		process(filename)
+		jobs <- filename
+	}
+	close(jobs)
+}
+
+func doJobs(done chan struct{}, results chan string, jobs chan string) {
+	for job := range jobs {
+		if result, ok := process(job); ok {
+			results <- result
+		}
+	}
+	done <- struct{}{}
+}
+
+func waitAndProcessResults(done chan struct{}, results chan string) {
+	for working := workers; working > 0; {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-done:
+			working--
+		}
 	}
 }
 
-func process(filename string) {
+func process(filename string) (string, bool) {
 	if info, err := os.Stat(filename); err != nil ||
 		(info.Mode()&os.ModeType != 0) {
-		return
+		return "", false
 	}
 	file, err := os.Open(filename)
 	if err != nil {
-		return
+		return "", false
 	}
 	defer file.Close()
 	config, _, err := image.DecodeConfig(file)
 	if err != nil {
-		return
+		return "", false
 	}
-	fmt.Printf(`<img src="%s" width="%d" height="%d" />`,
-		filepath.Base(filename), config.Width, config.Height)
-	fmt.Println()
+	return fmt.Sprintf(`<img src="%s" width="%d" height="%d" />`, filepath.Base(filename), config.Width, config.Height), true
 }
 
 func commandLineFiles(files []string) []string {
