9d29864e87b3a62b9aa84f2168414bfd9a8ce96c
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2021-11-22 10:54:57 +0300

    Bufio -> Ioutil
    Add possibility to rewrite string in file

--- Summerfield/sizeimages/sizeimages.go
+++ Summerfield/sizeimages/sizeimages.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 )
 
+// htmlImageFormat reads html-file and scans it for img-tags.
+// If in it is no width/height image data
+// htmlImageFormat calls getImageData func.
 func htmlImageFormat(filename string) {
-	file, err := os.Open(filename)
+	input, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		reTag := regexp.MustCompile(`<[iI][mM][gG][^>]+>`)
-		matchTag := reTag.FindStringSubmatch(scanner.Text())
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		reTag := regexp.MustCompile(`<[iI][mM][gG][^>]+>`) // Search img-tag
+		matchTag := reTag.FindStringSubmatch(line)
 		if len(matchTag) > 0 {
-			reAttr := regexp.MustCompile(`src=["’]([^"’]+)["’]`)
+			reAttr := regexp.MustCompile(`src=["’]([^"’]+)["’]`) //Search source from img-tag
 			matchAttr := reAttr.FindStringSubmatch(matchTag[0])
 			if len(matchAttr) >= 1 && strings.HasPrefix(matchAttr[1], "http") {
 				getImageData(matchAttr[1])
+				// If in img-tag no width/height attributes
+				// Write them right in html-file.
+				resW := strings.Contains(matchTag[0], "width")
+				if !resW {
+					lines[i] = "LOL"
+				}
+				resH := strings.Contains(matchTag[0], "height")
+				if !resH {
+					lines[i] = "LOL HEIGHT"
+				}
 			}
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(filename, []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
