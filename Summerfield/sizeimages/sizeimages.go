c70a060d8bff307b9b433ef939b9486df596d610
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2021-12-01 21:04:22 +0600

    Add main logic

--- Summerfield/sizeimages/sizeimages.go
+++ Summerfield/sizeimages/sizeimages.go
@@ -7,14 +7,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strconv"
 	"strings"
+)
+
+const (
+	w = "width"
+	h = "height"
 )
 
 // htmlImageFormat reads html-file and scans it for img-tags.
 // If in it is no width/height image data
 // htmlImageFormat calls getImageData func.
-func htmlImageFormat(filename string) {
+func htmlImageFormat(filename string) error {
 	input, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatalln(err)
@@ -29,16 +36,19 @@ func htmlImageFormat(filename string) {
 			reAttr := regexp.MustCompile(`src=["’]([^"’]+)["’]`) //Search source from img-tag
 			matchAttr := reAttr.FindStringSubmatch(matchTag[0])
 			if len(matchAttr) >= 1 && strings.HasPrefix(matchAttr[1], "http") {
-				getImageData(matchAttr[1])
+				width, height, err := getImageData(matchAttr[1])
+				if err != nil {
+					return err
+				}
 				// If in img-tag no width/height attributes
 				// Write them right in html-file.
-				resW := strings.Contains(matchTag[0], "width")
+				resW := strings.Contains(matchTag[0], w)
 				if !resW {
-					lines[i] = "LOL"
+					lines[i] = addHtmlAttr(line, w, width)
 				}
-				resH := strings.Contains(matchTag[0], "height")
+				resH := strings.Contains(matchTag[0], h)
 				if !resH {
-					lines[i] = "LOL HEIGHT"
+					lines[i] = addHtmlAttr(lines[i], h, height)
 				}
 			}
 		}
@@ -49,48 +59,53 @@ func htmlImageFormat(filename string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	return nil
 }
 
-func getImageData(link string) {
+// getImageData checks img-link and get image properties.
+// Rerurns img's width, height and error.
+func getImageData(link string) (int, int, error) {
+	var width, height int
 	// Get the data
 	resp, err := http.Get(link)
 	if err != nil {
-		log.Panic(err)
+		return width, height, err
 	}
 	defer resp.Body.Close()
 
-	config, format, err := image.DecodeConfig(resp.Body)
+	config, _, err := image.DecodeConfig(resp.Body)
+	if err != nil {
+		return width, height, err
+	}
+	return config.Width, config.Height, err
+}
+
+// addHtmlAttr adds in img-tag width or height attribute
+func addHtmlAttr(link string, attr string, value int) string {
+	s := strings.Split(link, `>`)
+	strValue := strconv.Itoa(value)
+	fullAttr := attr + "=" + `"` + strValue + `"`
+	fLink := strings.Join(s[:len(s)-1], "")
+	sLink := strings.Join(s[len(s)-1:], "")
+
+	return fLink + " " + fullAttr + ">" + sLink
+}
+
+func main() {
+	err := htmlImageFormat("test.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(link, format, config.Height, config.Width, config.ColorModel)
-	/*
-		// Create the file
-		out, err := os.Create(`C:\Users\aleks\Documents\GitHub\learngo\Summerfield\sizeimages\download.jpeg`)
-		if err != nil {
-			log.Panic(err)
+	argFiles := os.Args[1:] // Get filenames from command args
+	if len(argFiles) > 0 {
+		for _, filename := range argFiles {
+			htmlImageFormat(filename)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
-		defer out.Close()
-
-		// Write the body to file
-		_, err = io.Copy(out, resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		file, err := os.Open(out.Name())
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		config, format, err := image.DecodeConfig(bufio.NewReader(file))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(format, config.Height, config.Width, config.ColorModel)*/
-}
-
-func main() {
-	htmlImageFormat("test.html")
+	}
 }
