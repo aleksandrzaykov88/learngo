21c025915b47fcc2131b703b7c5635ce068b82fc
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2022-01-17 23:22:29 +0300

    gRPC server

---
+++ SmallCourses/gRPC/grpc-golang/server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "example.com/grpc/gen/proto"
+	"google.golang.org/grpc"
+)
+
+type TestApiServer struct {
+	pb.UnimplementedTestApiServer
+}
+
+func (s *TestApiServer) CreateUser(ctx context.Context, req *pb.User) (*pb.Response, error) {
+	msg := pb.Response{Msg: "Пользователь создан"}
+	return &msg, nil
+}
+
+func main() {
+	listner, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterTestApiServer(grpcServer, &TestApiServer{})
+
+	log.Println("Server on port 8080")
+	err = grpcServer.Serve(listner)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
