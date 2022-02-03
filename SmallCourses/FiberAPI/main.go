0864c764a14069299fb087a95df06466aebdd1af
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2022-02-03 12:09:00 +0300

    fiber DB connect

--- SmallCourses/FiberAPI/main.go
+++ SmallCourses/FiberAPI/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fiber_api/database"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,6 +12,7 @@ func welcome(c *fiber.Ctx) error {
 }
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/api", welcome)
