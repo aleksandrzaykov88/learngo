fd70e23a6992ce545a19c96fca7c289d81f2ee64
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2022-02-03 17:45:23 +0300

    Create and Get users

--- SmallCourses/FiberAPI/main.go
+++ SmallCourses/FiberAPI/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fiber_api/database"
+	"fiber_api/routes"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,11 +12,19 @@ func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to my awesome API")
 }
 
+func setupRoutes(app *fiber.App) {
+	// Welcome endopoint
+	app.Get("/api", welcome)
+	// User endpoints
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+}
+
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/api", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
