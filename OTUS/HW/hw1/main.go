1421febfe6fea606cc624bb6292ae1be260b5539
aleksandrzaykov88 <59988458+aleksandrzaykov88@users.noreply.github.com>
2022-02-04 17:40:11 +0300

    Second Lesson OTUS

---
+++ OTUS/HW/hw1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		os.Stderr.WriteString("ERROR: " + err.Error())
+	}
+	fmt.Println(time)
+}
